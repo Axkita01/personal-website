0739d45f6c38bf7070c1bbca1b7427ebb5c0d1bf
Axkita01 <94666562+Axkita01@users.noreply.github.com>
2022-06-30 22:38:33 -0500

    big navbar revamp

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,48 +3,33 @@ import '../Styles/nav.css';
 import pic from './logo.jpg';
 import linked from './linkedin.png';
 import git from './git.png';
-import {useState} from 'react';
-
-function Burger () {
-    const [burgerC, burgerClick] = useState(false);
-    switch(burgerC) {
-        
-        case (true):
-            return (
-            <div>
-                <label onClick = {() => burgerClick(false)} className = 'b' id = 'x'>   
-                    &#xd7;
-                </label>
-                <nav className = 'bc'>
-                    <ul id = 'clicked'>
-                        <li id = 'first-bl'><a className = 'link' id = 'bl' href = '/'>Home</a></li>
-                        <li id = 'bl2'><a className = 'link' id = 'bl' href = '/resume'>Resume</a></li>
-                        <li id = 'bl3'><a href = '/projects' className = 'link' id = 'bl'>Projects</a></li>
-                        <li id = 'last-bl'><a id = 'bl'className = 'link' href = '/about'>About</a></li>
-                    </ul>
-                 </nav>
-            </div>
-            );
-
-            case (false):
-                return ( 
-                    <label 
-                    onClick = {() => burgerClick(true)} 
-                    className = 'b' id = 'burger-start'
-                    > 
-                        <ul id = 'default'>
-                            <li className = 'line'></li>
-                            <li className = 'line'></li>
-                            <li className = 'line'></li>
-                        </ul>
-                    </label>
-                );
-    }           
-}
+import {useState, useEffect} from 'react';
 
 function Navbar () {
+    const [navClass, setClass] = useState('')
+    console.log(window.innerWidth)
+
+    function mount () {
+        window.onscroll = () => {
+            if (window.scrollY !== 0) {
+                setClass('scrolled')
+            }
+
+            else {
+                setClass('')
+            }
+        }
+    }
+    /*implement on unmount*/
+    function unmount () {
+        window.onscroll = null;
+    }
+    
+    useEffect(() => {
+        mount ()
+    }, [])
     return (
-        <div className = 'navbar'>
+        <div className = 'navbar' id = {navClass}>
             <a href = '/'>
                 <img 
                 className = 'logo' 
@@ -58,32 +43,46 @@ function Navbar () {
                     <li><a className = 'link' href = '/resume'>Resume</a></li>
                     <li><a href = '/projects' className = 'link'>Projects</a></li>
                     <li><a className = 'link' href = '/about'>About</a></li>
+                    <li><a 
+                        href = 'https://linkedin.com/in/alexander-f-kita'
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        >
+                        <img 
+                        className = 'icon' 
+                        id = 'linked' 
+                        src = {linked} 
+                        alt = 'logo'
+                        ></img>
+                        </a>
+                    </li>
+                    <li>
+                        <a 
+                        href = 'https://github.com/Axkita01'
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        >
+                            <img  
+                            className='icon'
+                            id = 'git' 
+                            src = {git} 
+                            alt = 'logo'
+                        ></img>
+            </a>
+                    </li>
                 </ul>
             </nav>
-            <a href = 'https://linkedin.com/in/alexander-f-kita'
-            target="_blank" 
-            rel="noopener noreferrer"
-            >
-                <img 
-                className = 'logo' 
-                id = 'linked' 
-                src = {linked} 
-                alt = 'logo'
-                ></img>
-            </a>
-            <a href = 'https://github.com/Axkita01'
-            target="_blank" 
-            rel="noopener noreferrer"
-            >
-                <img 
-                className = 'logo' 
-                id = 'git' 
-                src = {git} 
-                alt = 'logo'
-                ></img>
-            </a>
-            <Burger/>
+            <div className = 'mobile-menu'>
+                <button className = 'burger-toggle'>
+                    <ul className='lines'>
+                    <div className = 'line'/>
+                    <div className = 'line'/>
+                    <div className = 'line'/>  
+                    </ul>  
+                </button>
+            </div>
         </div>
+
     );
 }
 
