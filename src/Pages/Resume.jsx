a285d1c3c2afbcb7c122424164da98c9eecc7796
Axkita01 <94666562+Axkita01@users.noreply.github.com>
2022-09-20 19:35:28 -0500

    new carousel, removed mantine

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../Styles/resume.css';
-import PDF from '../assets/Alexander.Kita.ECE.Resume.Fall2022.pdf';
+import PDF from '../assets/Fall 2022 Resume Alex Kita';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5'
 import BottomSquares from '../Components/BottomDesign';
 
