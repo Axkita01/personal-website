5e3f445ae32b1ad1c5bb0ad3097bb8a0aa086206
Axkita01 <94666562+Axkita01@users.noreply.github.com>
2022-11-20 23:05:19 -0600

    projects update

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -10,7 +10,7 @@ function Projects() {
     let lswebsite_desc = "Reworked and added features to the official website of the super smash brothers organization's website at UT Austin."
     let speed_project_desc = "Created an app to play the card game called speed using React Native. Currently a work in progress and will continue to be improved."
     let personal_website_desc = "The current website being viewed. Portfolio Website for resume and projects."
-    let sweetTooth_desc = "Created iOS app utilizing google maps API to help users find dessert places in their area. Used React Native. Will eventually be deployed and web version is planned to be made."
+    let sweetTooth_desc = "Web app utilizing Yelp API to help users find dessert places in their area. Used ReactJS. Will eventually be deployed."
     return (
         <div className = 'page' style = {{paddingTop: 60}}>
             <Container className='first-half'>
@@ -65,7 +65,7 @@ function Projects() {
                 title = 'Sweet Tooth'
                 windowWidth = {window.innerWidth}
                 link = 'Not Deployed Yet.'
-                git = 'https://github.com/Axkita01/SweetTooth'/>
+                git = 'https://github.com/Axkita01/SweetTooth-Web'/>
             </Row>
 
         </div>
